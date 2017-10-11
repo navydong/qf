ecbe847b2b5940bcf19ef7b2e1a3ef1d928a4845
张鹏 <zhangpeng_bj@ysstech.com>
2017-10-11 21:00:31 +0800

    area-select

--- src/components/AreaSelector/index.js
+++ src/components/AreaSelector/index.js
@@ -1,10 +1,128 @@
 import React from 'react'
-import { Form, Row, Col, Input, Button, Icon, Card, Select } from 'antd'
-import { AreaData } from "./areaData";
+import { Form, Row, Col,  Select } from 'antd'
+
 
 const FormItem = Form.Item;
 const Option = Select.Option;
 
 class AreaDataSelector extends React.Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            province: "110000",
+            city: "110100",
+            county: "110101"
+        }
+    }
+
+    handleProvinceChange = (value) => {
+        console.log(value)
+        this.setState({
+            province: value,
+        })
+        console.log(this.state)
+    }
+
+    handleCityChange = (value) => {
+        console.log(value)
+        this.setState({
+            city: value
+        })
+        console.log(this.state)
+    }
+
+    handleCountryChange = (value) => {
+        this.setState({
+           county: value
+        })
+        console.log(this.state)
+    }
+
+    provinceOptions (){
+        let provs = [];
+        let data = this.props.data;
+        for( let item in data.provinces ){
+            provs.push([item,data.provinces[item].name])
+        }
+        return provs.map((value,key) => <Option key={key} value={value[0]}>{value[1]}</Option>)
+    }
+
+    cityOptions(){
+        let citys = [];
+        let data = this.props.data;
+        if( this.state.province ){
+            for( let item in data.provinces[this.state.province].citys ){
+                citys.push([item,data.provinces[this.state.province].citys[item].name])
+            }
+        }
+        return citys.map((value,key) => <Option key={key} value={value[0]}>{value[1]}</Option>)
+        console.log(citys)
+    }
+
+    countryOptions(){
+        let county = [];
+        let data = this.props.data;
+        if( this.state.province && this.state.city ){
+           // for( let item in data.provinces[this.state.province].citys[this.state.city].countys ){
+                //county.push([item,data.provinces[this.state.province].citys[this.state.city].countys[item].name])
+           // }
+        }
+
+        //return county.map(function(value,index){
+            //return (<Option key={index} value={value[0]}>{value[1]}</Option>)
+       // })
+    }
+
+    render(){
+        const data = this.props.data;
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span: 5 },
+            wrapperCol: { span: 19 },
+        };
+
+        return (
+            <div className="area-selector">
+                <Row gutter={16}>
+                    <Col md={8}>
+                        <FormItem {...formItemLayout} label={`省份`}>
+                            {getFieldDecorator('province',{
+                                //initialValue: data.provinces[this.state.province].name
+                            })(
+                                <Select  onChange={this.handleProvinceChange}>
+                                    {this.provinceOptions()}
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col md={8}>
+                        <FormItem {...formItemLayout} label={`城市`}>
+                            {getFieldDecorator('city',{
+                                //initialValue: data.provinces[this.state.province].citys[this.state.city].name
+                            })(
+                                <Select  onChange={this.handleCityChange}>
+                                    {this.cityOptions()}
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                    <Col md={8}>
+                        <FormItem {...formItemLayout} label={`区县`}>
+                            {getFieldDecorator('County',{
+                               //initialValue: data.provinces[this.state.province].citys[this.state.city].countys[this.state.county].name
+                            })(
+                                <Select  onChange={this.handleCountryChange}>
+                                    {this.countryOptions()}
+                                </Select>
+                            )}
+                        </FormItem>
+                    </Col>
+                </Row>
+            </div>
+        )
+    }
 
 }
+
+AreaDataSelector = Form.create()(AreaDataSelector)
+export default AreaDataSelector
--- src/views/organization/merchant.jsx
+++ src/views/organization/merchant.jsx
@@ -1,47 +1,20 @@
 import React from 'react'
 import BreadcrumbCustom from '../../components/BreadcrumbCustom';
-import { Form, Row, Col, Input, Button, Icon, Card, Select } from 'antd'
+import { Form, Row, Col, Input, Button,  Card } from 'antd'
+import AreaSelector from '../../components/AreaSelector'
+import { areaData } from '../../components/AreaSelector/areaData'
 const FormItem = Form.Item;
-const Option = Select.Option;
-const provinceData = ['Zhejiang', 'Jiangsu'];
-const cityData = {
-    Zhejiang: ['Hangzhou', 'Ningbo', 'Wenzhou'],
-    Jiangsu: ['Nanjing', 'Suzhou', 'Zhenjiang'],
-};
+
 class Merchant extends React.Component {
     state = {
         expand: false,
-        cities: cityData[provinceData[0]],
-        secondCity: cityData[provinceData[0]][0]
     };
-    handleSearch = (e) => {
-        e.preventDefault();
-        this.props.form.validateFields(( err,values ) => {
-            console.log('Received values of form', values)
-        })
-    }
-
-    handleProvinceChange = (value) => {
-        this.setState({
-            cities:cityData[value],
-            secondCity:cityData[value][0]
-        })
-    }
-
-    onSecondCityChange = (value) => {
-        this.setState({
-            secondCity: value
-        })
-    }
-
     render(){
         const { getFieldDecorator } = this.props.form;
         const formItemLayout = {
             labelCol: { span: 5 },
             wrapperCol: { span: 19 },
         };
-        const provinceOptions = provinceData.map(province => <Option key={province}>{province}</Option>);
-        const cityOptions = this.state.cities.map(city => <Option key={city}>{city}</Option>);
         return (
             <div className="merchant-wrapper">
                 <BreadcrumbCustom first="机构信息" second="商户" />
@@ -70,41 +43,7 @@ class Merchant extends React.Component {
                                 </FormItem>
                             </Col>
                         </Row>
-                        <Row gutter={16}>
-                            <Col md={8}>
-                                <FormItem {...formItemLayout} label={`省份`}>
-                                    {getFieldDecorator('province',{
-                                        initialValue: provinceData[0]
-                                    })(
-                                        <Select  onChange={this.handleProvinceChange}>
-                                            {provinceOptions}
-                                        </Select>
-                                    )}
-                                </FormItem>
-                            </Col>
-                            <Col md={8}>
-                                <FormItem {...formItemLayout} label={`城市`}>
-                                    {getFieldDecorator('city',{
-                                        initialValue: this.state.secondCity
-                                    })(
-                                        <Select  onChange={this.onSecondCityChange}>
-                                            {cityOptions}
-                                        </Select>
-                                    )}
-                                </FormItem>
-                            </Col>
-                            <Col md={8}>
-                                <FormItem {...formItemLayout} label={`区县`}>
-                                    {getFieldDecorator('County',{
-                                        initialValue: this.state.secondCity
-                                    })(
-                                        <Select  onChange={this.onSecondCityChange}>
-                                            {cityOptions}
-                                        </Select>
-                                    )}
-                                </FormItem>
-                            </Col>
-                        </Row>
+                        <AreaSelector data={areaData}/>
                         <Row gutter={16}>
                             <Col offset={22}>
                                 <Button type="primary" htmlType="submit">查询</Button>
