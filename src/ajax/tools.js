15f28f1e928aad4b15a01b91b38ca7036cdc7a80
donghaijun <donghaijun@ysstech.com>
2018-03-02 16:11:01 +0800

    添加NProgerss

--- src/ajax/tools.js
+++ src/ajax/tools.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: yss.donghaijun 
+ * @Date: 2018-03-01 16:40:54 
+ * @Last Modified by:   yss.donghaijun 
+ * @Last Modified time: 2018-03-01 16:40:54 
+ */
+
 import axios from 'axios'
 import qs from 'qs'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 
 axios.defaults.paramsSerializer = function (params) {
     return qs.stringify(params)
@@ -20,8 +29,16 @@ if (process.env.NODE_ENV !== 'production') {
 
 // axios.defaults.withCredentials = true;
 
+axios.interceptors.request.use(function (config) {
+    // 请求开始进度条开始
+    NProgress.start()
+    return config;
+});
+
 // 添加响应拦截器
 axios.interceptors.response.use(function (response) {
+    // 请求结束进度条开始
+    NProgress.done()
     //如果响应给了重新定向，这跳转到redirect的地址
     const redirect = /login$/i.test(response.request.responseURL)
     if (redirect) {
